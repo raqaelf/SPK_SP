1d8614dc3d40629d918d8e39d15759da70df9562
Raqael Fisabillah Ramadhan <raqaelf@student.uns.ac.id>
2021-10-15 23:27:56 +0700

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -27,8 +27,8 @@
 
                 <div class="card-body">
                     <ol>
-                        <li>(Dapat dilewati dan menggunakan data default) <b>Mengisi Data Kriteria</b> : digunakan sebagai dasar penilaian atau penetapan dalam menentukan Strageti Pembelajaran </li>
-                        <li>(Dapat dilewati dan menggunakan data default) <b>Mengisi Data Alternatif</b> : daftar Strageti Pembelajaran yang akan digunakan</li>
+                        <li>(Dapat dilewati dan menggunakan data default) <b>Mengisi Data Kriteria</b> : digunakan sebagai dasar penilaian atau penetapan dalam menentukan Strategi Pembelajaran </li>
+                        <li>(Dapat dilewati dan menggunakan data default) <b>Mengisi Data Alternatif</b> : daftar Strategi Pembelajaran yang akan digunakan</li>
                         <li><b>Mengisi Data Siswa</b> : daftar siswa beserta nilai bobot dari setiap kriteria </li>
                         <li><b>Hasil Perhitungan</b> : Melihat hasil rekomendasi strategi pembelajaran pada setiap anak dengan metode Simple Additive Weighting </li>
                     </ol>
